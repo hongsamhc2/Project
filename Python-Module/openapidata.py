f392bcc4bab762750f72e20c2963b0c7788fa54b
hongsamhc2 <69662531+hongsamhc2@users.noreply.github.com>
2020-11-03 01:00:25 +0900

    Add files via upload

--- Python-Module/openapidata.py
+++ Python-Module/openapidata.py
@@ -5,6 +5,7 @@ from urllib.request import Request, urlopen
 from urllib.parse import urlencode,quote_plus,unquote
 from datetime import datetime
 import os
+import UtilityFunc
 def get_data(url,queryParams):
     pattern = re.compile(('<([ㄱ-ㅣ가-힣]+)>'))
 
@@ -14,7 +15,6 @@ def get_data(url,queryParams):
     response_body = urlopen(request_url).read()
     soup = BeautifulSoup(response_body, 'lxml-xml')
     item_contents = soup.findAll('item')
-    print(soup.find('resultCode'))
     for content in item_contents:
 
         for i in range(len(content)):
@@ -34,14 +34,14 @@ def get_data(url,queryParams):
     return df
 
 
-code_list = ['11110']
+code_list = UtilityFunc.get_code()
 YM_list = []
 now = datetime.today()
 now_str = now.strftime('%Y%m')
 YM_list.append(now_str)
 now_int = int(now_str)
 prev = now_int -1
-for i in range(240):
+for i in range(120):
 
     if prev %100 ==0:
         prev = prev -100 +12
@@ -53,17 +53,32 @@ YM_list.pop(0)
 
 url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
 key = key = unquote('0RZwX%2BuD34DcTTK3adGbpiohmzlPjeJA26pc9Pqp0UFxQFi3mInkeBK8C%2Bklf8XlXSTyoEnmUQAeNmGMFUBFNw%3D%3D')
+df = pd.read_csv('.\\DB\\usedcode.csv')
 
+used_code = df['code'].to_string()
 for code in code_list:
+    if not os.path.isdir(f'.\\DB\\{code}'):
+        os.mkdir(f'.\\DB\\{code}')
+    if code in used_code:
+        continue
+
     for YM in YM_list:
-        if os.path.isfile(f'.\\DB\\{code}_{YM}.csv'):
+        if os.path.isfile(f'.\\DB\\{code}\\{code}_{YM}.csv'):
             continue
-        print(YM)
+
         queryParams = '?' + urlencode({ quote_plus('ServiceKey'):key,
                                         quote_plus('LAWD_CD'):code,
                                         quote_plus('DEAL_YMD'):YM})
         df = get_data(url,queryParams)
+        print(code, YM)
+
         if len(df) == 0:
-            break
-        df.to_csv(f'.\\DB\\{code}_{YM}.csv',encoding='utf-8-sig',index=False)
+            continue
+        print(df.head(1))
+        df.to_csv(f'.\\DB\\{code}\\{code}_{YM}.csv',encoding='utf-8-sig',index=False)
+    used_code.append(code)
+    print(used_code)
+    dff = pd.DataFrame(used_code,columns=['code'])
+    print(dff)
+    dff.to_csv('.\\DB\\usedcode.csv',index=False)
 
